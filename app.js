5aab3e97b836c87c5ea6361869229bfd842a2c30
Allan Miqueias <allan.santos12@hhotmail.com>
2025-02-01 23:05:30 -0300

    adição das primeiras funções

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo() {
+    let amigo = document.querySelector('input').value;
+    console.log('Amigo adicionado!');
+   
+}
+
+function sortearAmigo(params) {
+    let amigo = document.querySelector
+    
+}
+
+function limparCampo() {
+    nome = document.querySelector('input');
+    nome.value = '';
+}
